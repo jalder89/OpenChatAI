95381a4c1cced4742103345033140c8f15ec8d1a
Jessica Alder <jessalder1989@gmail.com>
2023-10-25 18:38:18 -0700

    Implemented routing and controllers for user signup and retreival. Retrieval currently only gets all users, single user retrieval remains todo. Implemented user data model for mongoDB.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
+// Initialize required imports
 config();
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+// DEV Middlware, remove in production
+app.use(morgan("dev"));
+
+app.use("/api/v1", appRouter);
 
 export default app;
---
+++ backend/src/models/User.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
+
+const chatSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        default: randomUUID,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["public", "private"],
+    },
+    name: {
+        type: String,
+        required: false,
+        min: 3,
+        max: 25,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+});
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    chats: [chatSchema],
+});
+
+export default mongoose.model("User", userSchema);
