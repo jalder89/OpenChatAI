6bb41d0b4b657d7762ab66c558b4c8921a57511a
Jessica Alder <jessalder1989@gmail.com>
2023-11-16 22:06:41 -0800

    Implemented Home UI with typing animation.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import TypingAnim from '../components/shared/typer/TypingAnim'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box width={"100%"} height={"100%"}>
+      <Box sx={{display: "flex", width: "100%", flexDirection: "column", alignItems: "center", mx: "auto", paddingTop: "37vh" }}>
+        <Box>
+          <TypingAnim />
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
